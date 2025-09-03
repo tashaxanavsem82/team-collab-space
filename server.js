84a0010328a6a598be20869922bd36448fab2765
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-03 12:14:06 -0500

    Fix: Update socket.io to use the latest version of the API.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
 app.use(cors());
 app.use(express.json());
